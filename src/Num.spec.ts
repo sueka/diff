3afb68df603caf3f63bf1dd97277daf21f62d2bb
sueka <mail@sueka.me>
2023-06-04 02:35:13 +0900

    :speech_balloon: Update test message

--- src/Num.spec.ts
+++ src/Num.spec.ts
@@ -3,7 +3,7 @@ import Num from './Num'
 describe('Num', () => {
   const it = new Num(2)
 
-  test('The derivative of it is zero', () => {
+  test('The derivative of Num is zero', () => {
     expect(it.diff().equals(new Num(0))).toBe(true)
   })
 })
--- src/Var.spec.ts
+++ src/Var.spec.ts
@@ -4,7 +4,7 @@ import Num from './Num'
 describe('Var', () => {
   const it = new Var('x')
 
-  test('The derivative of it is one', () => {
+  test('The derivative of Var is one', () => {
     expect(it.diff().equals(new Num(1))).toBe(true)
   })
 })
