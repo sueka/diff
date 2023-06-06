4cf918fea448034ba386ffbfbc03d0afa3888466
sueka <mail@sueka.me>
2023-06-06 20:26:04 +0900

    :sparkles: Make Var an Eq

--- src/Var.ts
+++ src/Var.ts
@@ -1,19 +1,32 @@
+import HashableEq from './HashableEq'
 import Num from './Num'
 
-export default class Var implements Expr, Diffible<Var, Num> {
+export default class Var extends HashableEq implements Expr, Diffible<Var, Num> {
   #letter: string
 
   declare _exprBrand: never
 
   constructor(letter: string) {
+    super()
+
     this.#letter = letter
   }
 
-  toString(): string {
+  override toString(): string {
     return this.#letter
   }
 
   diff() {
     return new Num(1)
+  }
+
+  hashCode() {
+    let result = 0
+
+    for (let i = 0; i < this.#letter.length; ++i) {
+      result = 31 * result + this.#letter.charCodeAt(i)
+    }
+
+    return result
   }
 }
