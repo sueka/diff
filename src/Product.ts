4d8a9e5cee201ccbe6229a5d750f51a1787a3cc5
sueka <mail@sueka.me>
2023-06-15 08:48:24 +0900

    :label: Reduce the number of type variables

--- src/Product.ts
+++ src/Product.ts
@@ -40,8 +40,8 @@ implements Expr, Diffible<
     this.#right = right
   }
 
-  static of<T>(left: T, right: Zero): Zero
-  static of<U>(left: Zero, right: U): Zero
+  static of(left: unknown, right: Zero): Zero
+  static of(left: Zero, right: unknown): Zero
   static of<T>(left: T, right: One): T
   static of<U>(left: One, right: U): U
   static of<T extends Expr, U extends Expr>(left: T, right: U): Product<T, U>
